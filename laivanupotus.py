88ee0d4e21d20f48b52e3da4cd5f37102beb912a
Koivunlehti <56386375+Koivunlehti@users.noreply.github.com>
2023-09-22 16:40:12 +0300

    Alustava laiva-luokka ja funktioita
    - Luotu alustava laiva-luokka ja sen funktioita

--- laivanupotus.py
+++ laivanupotus.py
@@ -9,6 +9,57 @@ LAIVA_1 = 1
 LAIVA_2 = 2
 LAIVA_3 = 3
 
+class Laiva():
+    def __init__(self, nimi:str, pituus:int=1, ruutu_koko:tuple=(20,20), vari:tuple=(100,100,100)) -> None:
+        self.nimi = nimi
+        self.pituus = pituus
+        self.vari = vari
+        self.ruutu_koko = ruutu_koko
+
+        self.vaaka = True
+        self.koordinaatit = []
+        self.osumat = []
+        self.tuhottu = False
+
+    def aseta(self, x:int, y:int, vaaka:bool = True) -> list:
+        self.koordinaatit = []
+        self.osumat = []
+        if vaaka:
+            for i in range(self.pituus):
+                self.koordinaatit.append((x + i, y))
+            self.vaaka = True
+        else:
+            for i in range(self.pituus):
+                self.koordinaatit.append((x, y + i))
+            self.vaaka = False
+        return self.koordinaatit
+    
+    def osuma(self, x, y):
+        if (x,y) in self.koordinaatit:
+            if (x,y) not in self.osumat:
+                self.osumat.append((x,y))
+        if len(self.osumat) == len(self.koordinaatit):
+            self.tuhottu = True
+
+    def piirra(self) -> pygame.Surface:
+        if self.vaaka:
+            laiva = pygame.Surface((self.ruutu_koko[0] * self.pituus, self.ruutu_koko[1]), pygame.SRCALPHA)
+            for i in range(len(self.koordinaatit)):
+                if self.koordinaatit[i] in self.osumat:
+                    pygame.draw.rect(laiva,(255,0,0),(self.ruutu_koko[0] * i,0,self.ruutu_koko[0],self.ruutu_koko[1]))
+                else:
+                    pygame.draw.rect(laiva,self.vari,(self.ruutu_koko[0] * i,0,self.ruutu_koko[0],self.ruutu_koko[1]))
+        else:
+            laiva = pygame.Surface((self.ruutu_koko[0], self.ruutu_koko[1] * self.pituus), pygame.SRCALPHA)
+            for i in range(len(self.koordinaatit)):
+                if self.koordinaatit[i] in self.osumat:
+                    pygame.draw.rect(laiva,(255,0,0),(0, self.ruutu_koko[1] * i, self.ruutu_koko[0], self.ruutu_koko[1]))
+                else:
+                    pygame.draw.rect(laiva,self.vari,(0, self.ruutu_koko[1] * i,self.ruutu_koko[0],self.ruutu_koko[1]))
+        
+        #laiva.fill(self.vari)
+        return laiva
+
 def peli():
     pygame.init()
 
@@ -21,6 +72,25 @@ def peli():
 
     # Käytössä olevat laivat
     laivat = [LAIVA_1,LAIVA_1,LAIVA_1,LAIVA_1,LAIVA_1,LAIVA_2,LAIVA_2,LAIVA_2,LAIVA_3,LAIVA_3]
+
+    #DEBUG
+    laiva = Laiva("nimi1",3)
+    print(laiva.aseta(0,0,True))
+    print(laiva.aseta(0,0,False))
+    #laiva.vaaka = True
+    laiva.osuma(0,1)
+    laiva.osuma(1,0)
+    print(laiva.osumat)
+    print(laiva.tuhottu)
+    laiva.osuma(0,0)
+    laiva.osuma(2,0)
+    #laiva.osuma(0,2)
+    print(laiva.osumat)
+    print(laiva.tuhottu)
+    # laiva = Laiva("nimi2",2)
+    # print(laiva.aseta(0,0,True))
+    # print(laiva.aseta(0,0,False))
+    #DEBUG LOPPU
 
     # Laivataulukot
     pelaajan_laivat = laivataulukon_alustus(20,20)
@@ -53,6 +123,11 @@ def peli():
         # Näytön tyhjennys
         naytto.fill((0, 0, 0))
 
+        # DEBUG
+        naytto.blit(laiva.piirra(),(10,10))
+        # DEBUG LOPPU
+
+        
         # Hiiren koordinaatit
         hiiri_x, hiiri_y = pygame.mouse.get_pos()
 
